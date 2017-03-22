1ae09ddf92daa22a22d130e4f2f35cf48d1282d0
Shade45 <Shade45vr@users.noreply.github.com>
2017-03-22 16:40:41 +0100

    Update settings.inc.php

--- includes/settings.inc.php
+++ includes/settings.inc.php
@@ -2,17 +2,17 @@
 
 // PDO socket
 // Or using socket would be even faster then 127.0.0.1 :3
-define('POMF_DB_CONN', 'mysql:host=127.0.0.1;dbname=12kmoe');
+define('POMF_DB_CONN', 'mysql:host=127.0.0.1;dbname=50mb');
 // MySQL user and password
 define('POMF_DB_USER', '');
 define('POMF_DB_PASS', '');
 
 // Root location of files
-define('POMF_FILES_ROOT', '/home/12kmoehost/public_html/12k.moe/files/');
+define('POMF_FILES_ROOT', '/home/root/public_html/50mb.ml/files/');
 // Maximum number of iterations while generating a new name
 define('POMF_FILES_RETRIES', 15);
 // URL to prepend to output (include trailing slash)
-define('POMF_URL', 'https://o.12k.moe/');
+define('POMF_URL', 'https://i.50mb.ml/');
 
 $doubledots = array_map('strrev', array(
 	'tar.gz',
